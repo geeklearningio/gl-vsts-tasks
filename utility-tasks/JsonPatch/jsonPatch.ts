e25d4c25cbe58ee8a53e4a26db99ff08e9ce888e
Cyprien Autexier <cyprien.autexier@gmail.com>
2016-05-05 18:05:23 +0200

    json patch

--- utility-tasks/JsonPatch/jsonPatch.ts
+++ utility-tasks/JsonPatch/jsonPatch.ts
@@ -7,7 +7,7 @@ var jsonPatch = require('fast-json-patch');
 
 var targetPath = tl.getPathInput("JsonWorkingDir");
 var patchContent = tl.getInput("JsonPatchContent");
-var customPaychSyntax = tl.getBoolInput("CustomPatchSyntax");
+var customPatchSyntax = tl.getBoolInput("CustomPatchSyntax");
 
 var patterns: any = tl.getInput("JsonTargetFilters").split("\n").map((pattern) => path.join(targetPath, pattern));
 var allFiles = tl.find(targetPath).map(file => path.resolve(file));
@@ -16,17 +16,21 @@ var files = allFiles.filter(micromatch.filter(patterns, { nodupes: true }));
 
 var varRegex = /\$\((.*)\)/g;
 var bomRegex = /^\uFEFF/;
+var optRegex = /^\?/;
 
 
 try {
 
-    patchContent = patchContent.replace(varRegex, (match, varName, offset, atring) => tl.getVariable(varName));
+    patchContent = patchContent.replace(varRegex, (match, varName, offset, string) => tl.getVariable(varName));
 
     for (var index = 0; index < files.length; index++) {
         var file = files[index];
+        console.log('Patching file :' + file);
+
         var fileContent = fs.readFileSync(file, { encoding: 'utf8' });
 
-        var hasBOM = fileContent.match(bomRegex).length > 0;
+        var hasBOMMatch = fileContent.match(bomRegex);
+        var hasBOM = hasBOMMatch && hasBOMMatch.length > 0;
 
         if (hasBOM) {
             console.log("file has bom");
@@ -38,28 +42,33 @@ try {
 
         var patches = [];
 
-        if (customPaychSyntax) {
+        if (customPatchSyntax) {
+            console.log("handling optional patches");
+
             patches = patchLines.map(line => {
-                if (line[0] = '?') {
-                    var patch = JSON.parse(line);
+                if (line.match(optRegex)) {
+                    var patch = JSON.parse(line.substr(1));
                     patch.ext_optional = true;
                     return patch;
                 }
                 return JSON.parse(line);
             });
 
+            console.log(JSON.stringify(patches));
+
             var validationResult = jsonPatch.validate(patches, json);
             if (validationResult && validationResult.length > 0) {
-                patches = patches.filter((patch, index) => {
-                    var patchValidation = validationResult[index];
+                patches = patches.filter((patch, patchIndex) => {
+                    var patchValidation = validationResult[patchIndex];
                     if (patchValidation) {
                         if (patch.ext_optional) {
-                            console.log('dropping optional patch ' + JSON.stringify(patch));
+                            console.log('dropping optional patch :' + JSON.stringify(patch));
                             return false;
                         } else {
                             throw new Error('a patch is not optional but its validation failed');
                         }
                     } else {
+                        console.log('keeping optional patch :' + JSON.stringify(patch));
                         return true;
                     }
                 });
@@ -68,12 +77,23 @@ try {
             patches = patchLines.map(line => JSON.parse(line));
         }
 
-        var patchedJson = jsonPatch.apply(patches, json, true);
-        var newFileContent = JSON.stringify(patchedJson);
-        if (hasBOM){
-            newFileContent = '\uFEFF' + newFileContent;
+        var prevalidate = jsonPatch.validate(patches, json);
+
+        if (prevalidate && prevalidate.length > 0) {
+
         }
-        fs.writeFileSync(file, newFileContent, { encoding: 'utf8' });
+
+        var result = jsonPatch.apply(json, patches, false);
+        if (result) {
+            var newFileContent = JSON.stringify(json);
+            if (hasBOM) {
+                newFileContent = '\uFEFF' + newFileContent;
+            }
+            fs.writeFileSync(file, newFileContent, { encoding: 'utf8' });
+            console.log('patch applied successfully');
+        } else {
+            throw new Error('Failed to apply patch')
+        }
     }
 
     tl.setResult(tl.TaskResult.Succeeded, "File Patched");
